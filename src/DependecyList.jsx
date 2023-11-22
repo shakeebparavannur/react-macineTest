39a15071270a4d449301a51042e5957b60c5a271
Shakeeb <shakeebparavannur@gmail.com>
2023-11-22 22:59:23 +0530

    optimize

--- src/DependecyList.jsx
+++ src/DependecyList.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const DependecyList = () => {
-    const [district,setDistrict] = useState([]);
-    const data = [
-        {state:'kerala',district:['Malappuram', 'Calicut', 'Ernamkulam']},
-        {state:'tamilnadu',district:['Coimbatore', 'Madurai', 'Erode']}
-    ]
-   const selectDist = (e)=>{
-        const state = data.find((da)=>da.state == e.target.value)
-        console.log(e.target.value,"eValue");
-        console.log(state,"state");
-        setDistrict(state.district)
-        console.log("clicked");
-    }
-    console.log(district);
-    
-    
+  const [district, setDistrict] = useState([]);
+  const data = [
+    { state: "kerala", district: ["Malappuram", "Calicut", "Ernamkulam"] },
+    { state: "tamilnadu", district: ["Coimbatore", "Madurai", "Erode"] },
+  ];
+  const selectDist = (e) => {
+    const state = data.find((da) => da.state == e.target.value);
+    console.log(e.target.value, "eValue");
+    console.log(state, "state");
+    setDistrict(state.district);
+    console.log("clicked");
+  };
+  console.log(district);
+
   return (
     <div>
-        <div>
-            <select name="state" id="" onChange={selectDist}>
-            {data.map((dat)=>{
-                console.log(dat.state);
-                return(
-                    <option value={dat.state} key={dat.state}>{dat.state}</option>
-                    
-                    )
-                    
-
-            })}
-            </select>
-            <select name="district" id="">
-                {district.map((dist)=>{
-                    return (
-                        <option key={dist} value={dist} >{dist}</option>
-                    )
-                })}
-            </select>
-        </div>
+      <div>
+        <select name="state" id="" onChange={selectDist}>
+          {data.map((dat) => {
+            console.log(dat.state);
+            return (
+              <option value={dat.state} key={dat.state}>
+                {dat.state}
+              </option>
+            );
+          })}
+        </select>
+        <select name="district" id="">
+          {district.map((dist) => {
+            return (
+              <option key={dist} value={dist}>
+                {dist}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DependecyList
+export default DependecyList;
