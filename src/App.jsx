6d145cb386e4b4508afc7e660097ff75ecd77500
Shakeeb <shakeebparavannur@gmail.com>
2023-11-24 10:29:21 +0530

    Add:Hide and Show Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import CheckBoxList from "./CheckBoxList";
 import DependecyList from "./DependecyList";
+import HideAndShow from "./HideAndShow";
 
 function App() {
   return (
     <>
       {/* <DependecyList/> */}
-      <CheckBoxList />
+      {/* <CheckBoxList /> */}
+      <HideAndShow/>
     </>
   );
 }
